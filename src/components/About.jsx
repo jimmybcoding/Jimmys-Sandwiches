acccbb635658596db89bfd4613ae4f21d56309af
James Barbour <jameskbarbour1@gmail.com>
2023-06-11 16:16:11 -0400

    test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,9 @@
-import hamilton from './images/hamilton.jpg'
-
 
 function About() {
   return (
     <div className="about-container">
       <p>Jimmy&apos;s Sandwhiches has been proud to serve Hamilton since 1990</p>
-      <img src={hamilton} className="hamilton-image"/>
+      <img src="./images/hamilton.jpg" className="hamilton-image"/>
       <p>Online ordering now available:</p>
       <div className="order-btn-div">
         <a href="https://www.tbdine.com" target="blank">Order Takeout</a>
